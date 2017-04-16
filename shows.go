7b954c67cabebe0db6c42aed64adca64947e13e8
Lucas BEE <pouulet@gmail.com>
2017-04-16 14:21:57 +0200

    Add explore functions for Shows

--- shows.go
+++ shows.go
@@ -1,6 +1,9 @@
 package trakttv
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Show holds the show informations
 type Show struct {
@@ -28,4 +31,104 @@ type Episode struct {
 	Title  string `json:"title"`
 	IDs    IDs    `json:"ids"`
 	Images Images `json:"images"`
+}
+
+// TrendingShow represents a trending show with the number of trakt.tv user
+// currently watching it
+type TrendingShow struct {
+	Watchers int  `json:"watchers"`
+	Show     Show `json:"show"`
+}
+
+// AnticipatedShow represents an anticipated show with the number of trakt.tv
+// user having the show in their wishlist
+type AnticipatedShow struct {
+	ListCount int  `json:"list_count"`
+	Show      Show `json:"show"`
+}
+
+// PlayedShow represents the show and its played count
+type PlayedShow struct {
+	WatcherCount   int  `json:"watcher_count"`
+	PlayCount      int  `json:"play_count"`
+	CollectedCount int  `json:"collected_count"`
+	Show           Show `json:"show"`
+}
+
+// PopularShows returns the popular shows
+func (t *TraktTv) PopularShows(qo QueryOption) ([]*Show, error) {
+
+	url := fmt.Sprintf("%s/%s", t.Endpoint, "shows/popular")
+
+	var shows []*Show
+	if err := t.request(url, NewQuery(qo), &shows); err != nil {
+		return nil, err
+	}
+
+	return shows, nil
+}
+
+// TrendingShows returns the trending shows
+func (t *TraktTv) TrendingShows(qo QueryOption) ([]*TrendingShow, error) {
+
+	url := fmt.Sprintf("%s/%s", t.Endpoint, "shows/trending")
+
+	var shows []*TrendingShow
+	if err := t.request(url, NewQuery(qo), &shows); err != nil {
+		return nil, err
+	}
+
+	return shows, nil
+}
+
+// AnticipatedShows returns the trending shows
+func (t *TraktTv) AnticipatedShows(qo QueryOption) ([]*AnticipatedShow, error) {
+
+	url := fmt.Sprintf("%s/%s", t.Endpoint, "shows/anticipated")
+
+	var shows []*AnticipatedShow
+	if err := t.request(url, NewQuery(qo), &shows); err != nil {
+		return nil, err
+	}
+
+	return shows, nil
+}
+
+// PlayedShows returns the mosts played shows
+func (t *TraktTv) PlayedShows(qo QueryOption) ([]*PlayedShow, error) {
+
+	url := fmt.Sprintf("%s/%s", t.Endpoint, "shows/played")
+
+	var shows []*PlayedShow
+	if err := t.request(url, NewQuery(qo), &shows); err != nil {
+		return nil, err
+	}
+
+	return shows, nil
+}
+
+// WatchedShows returns the mosts played shows by unique users
+func (t *TraktTv) WatchedShows(qo QueryOption) ([]*PlayedShow, error) {
+
+	url := fmt.Sprintf("%s/%s", t.Endpoint, "shows/watched")
+
+	var shows []*PlayedShow
+	if err := t.request(url, NewQuery(qo), &shows); err != nil {
+		return nil, err
+	}
+
+	return shows, nil
+}
+
+// CollectedShows returns the mosts collected shows
+func (t *TraktTv) CollectedShows(qo QueryOption) ([]*PlayedShow, error) {
+
+	url := fmt.Sprintf("%s/%s", t.Endpoint, "shows/collected")
+
+	var shows []*PlayedShow
+	if err := t.request(url, NewQuery(qo), &shows); err != nil {
+		return nil, err
+	}
+
+	return shows, nil
 }
